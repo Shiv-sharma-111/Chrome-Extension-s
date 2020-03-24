fc90d68f2eb2c1b6a24f1f480145cde7b76099a8
Shiv Sharma <shivsharmavictory@gmail.com>
2020-03-24 03:08:17 -0400

    Update Budget Manager

---
+++ Budget Manager/options.js
@@ -0,0 +1,27 @@
+$(function(){
+  chrome.storage.sync.get('limit', function(budget){
+    $('#limit').val(budget.limit);
+  })
+
+
+  $('#saveLimit').click(function(){
+    var limit = $('#limit').val();
+    if(limit)
+    {
+      chrome.storage.sync.set({'limit':limit},function(){
+        close();
+      });
+    }
+  });
+  $('#resetTotal').click(function(){
+    chrome.storage.sync.set({'total':0},function(){
+      var notifoptions = {
+        type:'basic',
+        iconUrl:'chrome48.png',
+        title: 'Total Reset!',
+        message:"Total has been to reset to zero"
+      };
+      chrome.notifications.create('limitNotif', notifoptions);
+    });
+  });
+});
--- Budget Manager/popup.js
+++ Budget Manager/popup.js
@@ -1,6 +1,12 @@
 $(function() {
+  chrome.storage.sync.get(['total','limit'], function() {
+    $('#total').text(budget.total);
+    $('#limit').text(budget.limit);
+  })
+
+
   $('#spendAmount').click(function(){
-    chrome.storage.sync.get('total',function(budget){
+    chrome.storage.sync.get(['total','limit'],function(budget){
       var newTotal = 0;
       if (budget.total) {
         newTotal+=parseInt(budget.total);
@@ -10,7 +16,17 @@ $(function() {
       {
         newTotal+=parseInt(amount);
       }
-      chrome.storage.sync.set('total':newTotal);
+      chrome.storage.sync.set({'total':newTotal},function(){
+        if(amount && newTotal>=budget.limit){
+          var notifoptions = {
+            type:'basic',
+            iconUrl:'chrome48.png',
+            title: 'Limit Reached!',
+            message:"Uh oh!Looks like your limit is over"
+          };
+          chrome.notifications.create('limitNotif', notifoptions);
+        }
+      });
 
       $('#total').text(newTotal);
       $('amount').val('');
