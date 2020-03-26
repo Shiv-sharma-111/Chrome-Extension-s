d63fbd9a6a557c428f0e670767b2f9c9066e78e5
Shiv Sharma <shivsharmavictory@gmail.com>
2020-03-26 10:39:52 -0400

    Badge added

--- Budget Manager/eventPage.js
+++ Budget Manager/eventPage.js
@@ -37,3 +37,8 @@ chrome.contextMenus.onClicked.addListener(function(clickdata){
     }
   }
 });
+
+
+chrome.storage.onChanged.addListener(function(changes, storageName){
+  chrome.browserAction.setBadgeText({"text": changes.total.newValue.toString()});
+});
